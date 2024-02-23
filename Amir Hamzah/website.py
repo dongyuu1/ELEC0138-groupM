5642bb9177893163c65d1c5e2f5f19a57dc2134a
Amir Hamzah <test@gmail.com>
2024-02-23 12:03:28 +0000

    added file

--- Amir Hamzah/website.py
+++ Amir Hamzah/website.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template
 test = Flask(__name__)
 
-@test.route('/html')
+@test.route('/')
 def home():
    return render_template('index.html')
-
-test.config['TEMPLATES_AUTO_RELOAD'] = True
 
 if __name__ == '__main__':
     test.run()
