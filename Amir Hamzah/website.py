945be00e5385138df66611580bb7db2ea3a73783
Amir Hamzah <test@gmail.com>
2024-02-23 12:07:59 +0000

    folder Amir Hamzah re-added

--- Amir Hamzah/website.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-test = Flask(__name__)
-
-@test.route('/')
-def home():
-   return render_template('index.html')
-
-if __name__ == '__main__':
-    test.run()
