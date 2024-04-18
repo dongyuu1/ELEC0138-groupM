e7675d4020397b5185182db541cc0b63eb4d286e
dongyuu1 <147731990+dongyuu1@users.noreply.github.com>
2024-04-19 04:07:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from Carpark_App_new.client.Carpark_App import app
 from Carpark_App_new.server.insert_data import insert
 if __name__ == "__main__":
     # insert() is for generating simulated data for an empty database.
-    # The data has already contained in the sql file
+    # The data has already been contained in the sql file
     # insert()
     app.run(host='0.0.0.0')
